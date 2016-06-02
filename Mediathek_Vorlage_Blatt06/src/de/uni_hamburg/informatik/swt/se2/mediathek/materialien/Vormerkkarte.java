873583f4fd7966f6c794782e0470d8562aeab335
Tim Pietz <t.pietz97@gmail.com>
2016-06-02 23:29:40 +0200

    Aus Übung importieren

---
+++ Mediathek_Vorlage_Blatt06/src/de/uni_hamburg/informatik/swt/se2/mediathek/materialien/Vormerkkarte.java
@@ -0,0 +1,24 @@
+package de.uni_hamburg.informatik.swt.se2.mediathek.materialien;
+
+import de.uni_hamburg.informatik.swt.se2.mediathek.materialien.medien.Medium;
+
+public class Vormerkkarte
+{
+    
+    private final Kunde _kunde;
+    private final Medium _medium;
+    
+    public Vormerkkarte(Kunde kunde, Medium medium) {
+        _kunde = kunde;
+        _medium = medium;
+    }
+    
+    public Kunde getVormerker() {
+        return _kunde;
+    }
+    
+    public Medium getMedium() {
+        return _medium;
+    }
+    
+}
