8148e075ee2e8697356843c71ef612ba7ee69ead
Tim Pietz <t.pietz97@gmail.com>
2016-06-10 14:53:57 +0200

    Schnittstellenkommentare Beobachtermuster

--- Kinoticketverkauf_Vorlage_Blatt07/src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/beobachtermuster/Beobachtbar.java
+++ Kinoticketverkauf_Vorlage_Blatt07/src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/beobachtermuster/Beobachtbar.java
@@ -3,20 +3,34 @@ package de.uni_hamburg.informatik.swt.se2.kino.werkzeuge.beobachtermuster;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Implementation von Observable des Observable-Patterns
+ */
 public abstract class Beobachtbar
 {
     private List<Beobachter> _beobachter;
 
+    /**
+     * Initialisert den Beobachtbar
+     */
     public Beobachtbar()
     {
         _beobachter = new ArrayList<>();
     }
 
+    /**
+     * Fügt einen zu benachrichtigenden Beobachter hinzu
+     * 
+     * @param beobachter    Neuer Beobachter dieser Klasse
+     */
     public void registriereBeobachter(Beobachter beobachter)
     {
         _beobachter.add(beobachter);
     }
 
+    /**
+     * Benachrichtigt die Beobachter dieser Klasse über eine Änderung
+     */
     public void informiereUeberAenderung()
     {
         for (Beobachter beobachter : _beobachter)
--- Kinoticketverkauf_Vorlage_Blatt07/src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/beobachtermuster/Beobachter.java
+++ Kinoticketverkauf_Vorlage_Blatt07/src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/beobachtermuster/Beobachter.java
@@ -1,6 +1,15 @@
 package de.uni_hamburg.informatik.swt.se2.kino.werkzeuge.beobachtermuster;
 
+
+/**
+ * Implementation von Observer des Observable-Patterns
+ */
 public interface Beobachter
 {
+    /**
+     * Wird aufgerufen, wenn ein beobachtetes eine Änderung vorgenommen hat
+     * 
+     * @param beobachtbar   Beobachtbar der eine Änderung vorgenommen hat
+     */
     void reagiereAufAenderung(Beobachtbar beobachtbar);
 }
